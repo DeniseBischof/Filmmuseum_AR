a1757b20c94482113dff3dc0a2e7b4266f85d620
Denise Bischof <33006225+DeniseBischof@users.noreply.github.com>
2022-05-31 14:44:41 +0200

    add Machine build test

---
+++ AR_Template/Assets/Scripts/TouchController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    private bool isMoveing = false;
+    private float hitDisstance = 0.0f;
+
+    public string moveTarget = "submarine_merged_adapted";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+         {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(touchPosition.x, touchPosition.y, 0f));
+
+            RaycastHit hit;
+            RaycastHit[] targetHits;
+  
+            if(isMoveing)
+            {
+                Vector3 targetPosition = ray.direction * hitDisstance + ray.origin;
+                transform.position = targetPosition;
+
+                targetHits = Physics.RaycastAll(ray);
+
+                foreach(var targetHit in targetHits)
+                {
+                    Debug.Log(targetHit.collider.name);
+                    Debug.Log(targetHit.transform.name);
+
+                    if(targetHit.transform.name == moveTarget)
+                    {
+                        gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                Physics.Raycast(ray.origin, ray.direction, out hit);
+                if (hit.transform != null)
+                {
+                    isMoveing = true;
+                    hitDisstance = hit.distance;
+                }
+            }
+        }
+    }
+}
